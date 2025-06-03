1f112e5cdc72500e0db2c1d390821aee7ee7083d
Ashwini Bisen <ashwinibisen26@gmail.com>
2025-06-03 23:51:00 +0530

    Feature Update-Navigation

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/SQLquery')
+def sqlquery():
+    return render_template('SQLquery.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
